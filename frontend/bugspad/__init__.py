8d567e3ddc65efa90076fa088a5c01920bc6bbab
Souradeep De <souradeep.2011@gmail.com>
2013-09-22 23:58:19 +0530

    Added Bug Comment System using AJAX

--- frontend/bugspad/__init__.py
+++ frontend/bugspad/__init__.py
@@ -10,9 +10,10 @@ from flask_fas_openid import FAS
 from .bsession import RedisSessionInterface
 from .forms import BugForm
 from .utils import BugspadBackendAPI
-
+import requests
 # import forms as forms
-
+from datetime import datetime
+from time import strftime
 
 # Create the application.
 APP = flask.Flask(__name__)
@@ -80,6 +81,32 @@ def bug_details(bug_id):
 @APP.route('/bug/<int:bug_id>/edit', methods=('GET', 'POST'))
 @login_required
 def bug_edit(bug_id):
+    if flask.request.method == 'POST' and flask.request.is_xhr == True:
+        comment_text = flask.request.values['comment']
+        json_data = {'user': flask.g.fas_user.fullname,
+                     'password' : '',
+                     'bug_id' : bug_id,
+                     'desc' : comment_text
+        }
+        requests.post('http://127.0.0.1:9998/comment', json_data)
+        post_time = datetime.now().strftime('%Y-%m-%d %X')
+        no = flask.request.values['comment_number']
+        comment_html = '''<div class="new-comment accordion" id="comments-accordion">
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" href="#comment%s" style="background-color:#f5f5f5">
+                    Description by <strong>%s</strong> <span class="pull-right">%s</span>
+                </a>
+            </div>
+            <div id="comment%s" class="accordion-body collapse in">
+                <div class="accordion-inner">
+                    %s
+                </div>
+            </div>
+        </div>
+    </div>''' % (no, flask.g.fas_user.fullname, post_time, no, comment_text)
+        return flask.make_response(flask.jsonify({'post':True ,'comment_html': comment_html }), 200)
+
     api_obj = BugspadBackendAPI()
     bug_details = api_obj.bug_details(bug_id)
     form = BugForm(initial_data=bug_details, product_id=1)
